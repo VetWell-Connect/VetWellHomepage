0cfa9cf45c8636ec19e6c79ad26b4178e430fc8a
pswishy <piercemedy24@gmail.com>
2024-02-29 20:20:00 -0500

    create map component

--- homepage/src/Pages/Home.js
+++ homepage/src/Pages/Home.js
@@ -1,10 +1,15 @@
 import React from 'react-bootstrap';
 import InfoCarousel from '../components/AppCarousel';
+import MapComponent1 from '../components/Map';
+//import MapComponent2 from '../components/MapComponent';
+// import Map3 from '../components/Map3';
 
 function Home() {
     return (
         <div>
             <InfoCarousel/>
+            <MapComponent1/>
+
         </div>
     );
 }
